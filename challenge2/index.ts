bef24b904f4f1f5edbc8345549c91b3bf5c183ea
juwonkang <wndnjs1100@gmail.com>
2024-02-13 00:26:58 +0900

    secondcommit

--- challenge2/index.ts
+++ challenge2/index.ts
@@ -1,11 +1,55 @@
+// 인터페이스 정의
 interface MyApiHandler<T> {
   fetchData(endpoint: string): Promise<T>
 }
-class MyApiHandler<T> implements MyApiHandler<T> {
-  private baseUrl: string
+
+// 클래스 정의
+class ApiHandler<T> implements MyApiHandler<T> {
+  private baseUrl: string // base url (ex. https://api.somedomain.com)
+
   constructor(baseUrl: string) {
-    this.baseUrl = base
+    this.baseUrl = baseUrl
   }
 
-  fetchData(endpoint: string): Promise<T> {}
+  // endpoint (ex. /users, /posts, / profiles)
+  async fetchData(endpoint: string): Promise<T> {
+    try {
+      const url = this.baseUrl + endpoint
+      const response = await fetch(url)
+
+      if (!response.ok) {
+        throw new Error(`${response.status}`)
+      }
+
+      const data: T = await response.json()
+
+      return data
+    } catch (error) {
+      throw new Error(`에러 발생 : ${error}`)
+    }
+  }
 }
+
+type Post = {
+  id: number
+  userId: number
+  title: string
+  body: string
+}
+
+// 메인 함수
+async function main() {
+  // https://jsonplaceholder.typicode.com/posts
+  const baseUrl = 'https://jsonplaceholder.typicode.com'
+  const apiHandler = new ApiHandler<Post[]>(baseUrl)
+
+  try {
+    const posts: Post[] = await apiHandler.fetchData('/posts')
+
+    posts.forEach((post) => console.log(post))
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+main()
